db7a585be618ab608f578ee5137a9c5313b31f33
quan1605 <nquan0580@gmail.com>
2022-10-27 19:38:57 +0700

    ud1

---
+++ RAZOR/Data/RAZORContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RAZOR.Models;
+
+namespace RAZOR.Data
+{
+    public class RAZORContext : DbContext
+    {
+        public RAZORContext (DbContextOptions<RAZORContext> options)
+            : base(options)
+        {
+        }
+        
+        public DbSet<RAZOR.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ RAZOR/Pages/Movies/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RAZOR.Data;
+using RAZOR.Models;
+
+namespace RAZOR.Pages.Movies
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly RAZOR.Data.RAZORContext _context;
+
+        public DetailsModel(RAZOR.Data.RAZORContext context)
+        {
+            _context = context;
+        }
+
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movie.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Movie == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
